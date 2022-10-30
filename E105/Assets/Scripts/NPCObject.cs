600e6a8e891358e42fbfee09e56dd2e6d80d6505
jimin6206 <jimin6206@gmail.com>
2022-10-31 00:18:59 +0900

    feat : Animals 추가

--- E105/Assets/Scripts/NPCObject.cs
+++ E105/Assets/Scripts/NPCObject.cs
@@ -8,29 +8,43 @@ public class NPCObject : MonoBehaviour
     // Start is called before the first frame update
 
     public bool _useKey_status = false;
-    public Transform _Player;
     private Animator _animator;
     public GameObject _PlayerObject;
-    public GameObject _UI;
-    public bool _talk;
+    public GameObject _conversationUI;
 
     void Start()
     {
         _animator = GetComponent<Animator>();
+        _conversationUI.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        NPCInteraction();
+    }
+
+    private void NPCInteraction()
+    {
         if(_useKey_status && Input.GetButtonDown("interactionKey"))
         {
             _animator.SetTrigger("move");
-            this.transform.LookAt(_Player);
-            Debug.Log("UI 띄우기");
-           
-            _PlayerObject.GetComponent<PlayerSystem>()._canMove = false;
-            _UI.SetActive(true);
+            this.transform.LookAt(_PlayerObject.transform);
+            
+            ConversationStart();
         }
+    }
+
+    public void ConversationStart()
+    {
+        _PlayerObject.GetComponent<PlayerSystem>()._canMove = false;
+        _conversationUI.SetActive(true);
+    }
+
+    public void ConversationEnd()
+    {
+        _conversationUI.SetActive(false);
+        _PlayerObject.GetComponent<PlayerSystem>()._canMove = true;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -39,7 +53,6 @@ public class NPCObject : MonoBehaviour
         {
             Debug.Log("플레이어 들어옴");
             _useKey_status = true;
-           
         }
     }
 
@@ -49,7 +62,6 @@ public class NPCObject : MonoBehaviour
         {
             Debug.Log("플레이어 나감");
             _useKey_status = false;
-           
         }
     }
 }
