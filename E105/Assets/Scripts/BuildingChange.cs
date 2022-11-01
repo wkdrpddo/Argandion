dadb257718599e8c852b9695882ec2f3d5c594b6
박주연 <18cwceark@gmail.com>
2022-11-01 14:54:40 +0900

    feat: 건축물 바꾸기 구현

--- E105/Assets/Scripts/BuildingChange.cs
+++ E105/Assets/Scripts/BuildingChange.cs
@@ -4,24 +4,32 @@ using UnityEngine;
 
 public class BuildingChange : MonoBehaviour
 {
-    public GameObject[] _buildings = new GameObject[3];
+    // public GameObject[] _buildings;
     public bool _underConstruction;
     public int _phase = -1;
     public MakeBuildingFence _makeFences;
 
-    public GameObject _constructionSet;
+    public GameObject _constructionPrefab;
     public GameObject _buildingPlace;
+
+    private GameObject _constructionSet;
 
     // 테스트용 변수
     public bool _buildStartTest;
+    public bool _dayEnd;
 
     // 테스트용 update();
     void Update()
     {
         if (_buildStartTest)
         {
-            BuildStart();
             _buildStartTest = false; // 테스트용 변수
+            BuildStart();
+        }
+        if (_dayEnd)
+        {
+            DayEnd();
+            _dayEnd = false;
         }
 
     }
@@ -34,10 +42,13 @@ public class BuildingChange : MonoBehaviour
         float constructionY = _buildingPlace.transform.position.y + -9.42f;
         float constructionZ = _buildingPlace.transform.position.z + 2.38f;
         _underConstruction = true; // 건축 시작
-        _phase = 0; // 0단계
+        _phase = 1; // 1단계 (펜스에서 자재)
         // 중간 건축 생성
         Debug.Log(_makeFences._colliderCenter);
-        Instantiate(_constructionSet, new Vector3(constructionX, constructionY, constructionZ), Quaternion.identity, _buildingPlace.transform);
+        _constructionSet = Instantiate(_constructionPrefab, new Vector3(constructionX, constructionY, constructionZ), Quaternion.identity, _buildingPlace.transform);
+        Debug.Log(_constructionSet);
+        // 배열 저장
+        // _buildings = _constructionSet.transform.GetChild(); //
         Destroy(_makeFences._fences);
     }
 
@@ -46,13 +57,30 @@ public class BuildingChange : MonoBehaviour
     // 건축 마지막 날이면 Destory()
     public void DayEnd()
     {
-
+        if (_underConstruction)
+        {
+            // 자재에서 돌 벽, 돌 벽에서 벽
+            if (_phase == 1 || _phase == 2)
+            {
+                updateObject(_phase++);
+            }
+            // 벽에서 완성
+            else if (_phase == 3)
+            {
+                updateObject(_phase);
+                Destroy(_constructionSet);
+            }
+        }
     }
 
-    // 건물 오브젝트 바꾸기
+    // 건물 오브젝트 바꾸기 (현재 단계 들어옴)
     public void updateObject(int index)
     {
+        Debug.Log("index: " + index);
         // 중간 건축물 바꾸기
+        _constructionSet.transform.GetChild(index - 1).gameObject.SetActive(false);
+        _constructionSet.transform.GetChild(index).gameObject.SetActive(true);
+
 
         // 최종 건축물로 바꾸기
 
