d01e7f2b7736efd8798a31748f7d4008dee26718
박주연 <18cwceark@gmail.com>
2022-11-03 15:28:46 +0900

    feat: Bigger construction place coliider 제거

--- E105/Assets/Scripts/MakeBuildingFence.cs
+++ E105/Assets/Scripts/MakeBuildingFence.cs
@@ -92,8 +92,8 @@ public class MakeBuildingFence : MonoBehaviour
         GameObject sign = Instantiate(_sign, new Vector3(_colliderCenter.x, -0.5f, _colliderCenter.z - _colliderSize.z / 2 - 0.5f), Quaternion.Euler(0, -180, 0), _fences.transform.parent.gameObject.transform);
         // 아이콘 바꾸기
         sign.transform.GetChild(0).gameObject.GetComponent<MeshRenderer>().material = _icon;
-        //  
-        sign.GetComponent<InteractionUI>()._uiGroup = _uiSet;
+        // 표지판 상호작용 
+        // sign.GetComponent<InteractionUI>()._uiGroup = _uiSet;
 
     }
 }
