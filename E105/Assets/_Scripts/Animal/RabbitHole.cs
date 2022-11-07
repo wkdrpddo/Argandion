c388c12a1e6db0ee2a8597da187cb2ca584cc4cf
jimin6206 <jimin6206@naver.com>
2022-11-08 01:18:32 +0900

    feat : animal 확률 계산

--- E105/Assets/_Scripts/Animal/RabbitHole.cs
+++ E105/Assets/_Scripts/Animal/RabbitHole.cs
@@ -7,18 +7,18 @@ public class RabbitHole : MonoBehaviour
     [SerializeField] public GameObject rabbit1;
     [SerializeField] public GameObject rabbit2;
     [SerializeField] public GameObject rabbit3;
+    [SerializeField] public int num;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < 1; i++)
+        for (int i = 0; i < num; i++)
         {
             Instantiate(rabbit1, transform.position, Quaternion.identity).transform.parent = this.transform;
-            Instantiate(rabbit2, transform.position, Quaternion.identity).transform.parent = this.transform;
-            Instantiate(rabbit3, transform.position, Quaternion.identity).transform.parent = this.transform;
+
         }
-    
+
     }
 
     // Update is called once per frame
--- E105/Assets/_Scripts/_System/PointSystem.cs
+++ E105/Assets/_Scripts/_System/PointSystem.cs
@@ -18,39 +18,39 @@ public class PointSystem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void DayEnd()
     {
         Debug.Log("Point sys dayend");
-        switch(_isDen)
+        switch (_isDen)
         {
             case 0:
                 Check_Animal();
                 break;
             case 1:
-                Spawn_Animal(0,1);
+                Spawn_Animal(0, 1);
                 break;
             case 2:
-                Spawn_Animal(1,1);
+                Spawn_Animal(1, 1);
                 break;
             case 3:
-                Spawn_Animal(2,1);
+                Spawn_Animal(2, 1);
                 break;
             case 4:
-                Spawn_Animal(3,1);
+                Spawn_Animal(3, 1);
                 break;
         }
     }
 
     private void Check_Animal()
     {
-        float srnd = Random.Range(0f,1f);
+        float srnd = Random.Range(0f, 1f);
         if (srnd <= _Sector._animal_spawn_base_percent)
         {
             Debug.Log("동물 소환되며 rnd는");
-            int rnd = Random.Range(0,_Sector._animal_type_base_factor[3]);
+            int rnd = Random.Range(0, _Sector._animal_type_base_factor[3]);
             Debug.Log(rnd);
             int len = 0;
             int sumV = 0;
@@ -58,63 +58,64 @@ public class PointSystem : MonoBehaviour
             if (rnd < _Sector._animal_type_base_factor[0])
             {
                 len = _Sector._animal_bunny_count_factor.Length;
-                sumV = Random.Range(0,_Sector._animal_bunny_count_factor[len-1]);
+                sumV = Random.Range(0, _Sector._animal_bunny_count_factor[len - 1]);
                 Debug.Log(sumV);
                 Debug.Log("토끼 카운트");
-                while (!(_Sector._animal_bunny_count_factor[index]<=sumV && sumV<=_Sector._animal_bunny_count_factor[index+1]))
+                while (!(_Sector._animal_bunny_count_factor[index] <= sumV && sumV <= _Sector._animal_bunny_count_factor[index + 1]))
                 {
-                    index+=1;
+                    index += 1;
                 }
                 Debug.Log(index);
-                Spawn_Animal(0,index+1);
+                Spawn_Animal(0, index + 1);
             }
             else if (rnd < _Sector._animal_type_base_factor[1])
             {
                 len = _Sector._animal_deer_count_factor.Length;
-                sumV = Random.Range(0,_Sector._animal_deer_count_factor[len-1]);
+                sumV = Random.Range(0, _Sector._animal_deer_count_factor[len - 1]);
                 Debug.Log(sumV);
                 Debug.Log("순록 카운트");
-                while (!(_Sector._animal_deer_count_factor[index]<=sumV && sumV<=_Sector._animal_deer_count_factor[index+1]))
+                while (!(_Sector._animal_deer_count_factor[index] <= sumV && sumV <= _Sector._animal_deer_count_factor[index + 1]))
                 {
-                    index+=1;
+                    index += 1;
                 }
                 Debug.Log(index);
-                Spawn_Animal(1,index+1);
+                Spawn_Animal(1, index + 1);
             }
             else if (rnd < _Sector._animal_type_base_factor[2])
             {
                 len = _Sector._animal_wolf_count_factor.Length;
-                sumV = Random.Range(0,_Sector._animal_wolf_count_factor[len-1]);
+                sumV = Random.Range(0, _Sector._animal_wolf_count_factor[len - 1]);
                 Debug.Log(sumV);
                 Debug.Log("늑대 카운트");
-                while (!(_Sector._animal_wolf_count_factor[index]<=sumV && sumV<=_Sector._animal_wolf_count_factor[index+1]))
+                while (!(_Sector._animal_wolf_count_factor[index] <= sumV && sumV <= _Sector._animal_wolf_count_factor[index + 1]))
                 {
-                    index+=1;
+                    index += 1;
                 }
                 Debug.Log(index);
-                Spawn_Animal(2,index+1);
+                Spawn_Animal(2, index + 1);
             }
             else if (rnd < _Sector._animal_type_base_factor[3])
             {
                 len = _Sector._animal_bear_count_factor.Length;
-                sumV = Random.Range(0,_Sector._animal_bear_count_factor[len-1]);
+                sumV = Random.Range(0, _Sector._animal_bear_count_factor[len - 1]);
                 Debug.Log(sumV);
                 Debug.Log("곰 카운트");
-                while (!(_Sector._animal_bear_count_factor[index]<=sumV && sumV<=_Sector._animal_bear_count_factor[index+1]))
+                while (!(_Sector._animal_bear_count_factor[index] <= sumV && sumV <= _Sector._animal_bear_count_factor[index + 1]))
                 {
-                    index+=1;
+                    index += 1;
                 }
                 Debug.Log(index);
-                Spawn_Animal(3,index+1);
+                Spawn_Animal(3, index + 1);
             }
         }
     }
 
-    private void Spawn_Animal(int index,int count)
+    private void Spawn_Animal(int index, int count)
     {
-        Vector3 pos = new Vector3(_base_transform.position.x,_base_transform.position.y+4f,_base_transform.position.z);
-        for(int i=0;i<count;i++) {
-            Instantiate(_animals[index],pos,new Quaternion());
+        Vector3 pos = new Vector3(_base_transform.position.x, _base_transform.position.y, _base_transform.position.z);
+        for (int i = 0; i < count; i++)
+        {
+            Instantiate(_animals[index], pos, new Quaternion());
         }
     }
 }
